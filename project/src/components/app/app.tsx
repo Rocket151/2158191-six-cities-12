0eb70aa32deabac1f7af3d89dbcc8c335806b3a0
Vladislav <strizhovsaglvladagl@gmail.com>
2023-03-09 06:13:35 +0000

    На этом шаге добавим маршрутизацию в приложение. Для этого в модуле компонента `App` подключим несколько компонентов из пакета `react-router-dom` (`Routes`, `Route`, `BrowserRouter`), а затем обновим код самого компонента: добавим маршрутизацию.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import OfferScreen from '../../pages/offer-screen/offer-screen'
 import MainScreen from '../../pages/main-screen/main-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen'
 
 type AppScreenProps = {
   adCount: number;
@@ -6,6 +12,29 @@ type AppScreenProps = {
 
 export default function App({adCount}: AppScreenProps): JSX.Element {
   return (
-    <MainScreen adCount={adCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element = {<MainScreen adCount={adCount} />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element = {<FavoritesScreen />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element = {<LoginScreen />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element = {<OfferScreen />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>    
   );
 }
