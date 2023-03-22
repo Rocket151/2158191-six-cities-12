a636d585fb184276ad568eb3bec1d06c0dae23b3
Vladislav <strizhovsaglvladagl@gmail.com>
2023-03-22 04:08:24 +0000

    Создаст компонент map и подключит его в компонент main-screen

--- project/src/const.tsx
+++ project/src/const.tsx
@@ -20,3 +20,9 @@ export enum AuthorizationStatus {
 }
 
 export const RATING_STARS_STYLE_KOEF = 20;
+
+export const URL_MARKER_DEFAULT =
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+
+export const URL_MARKER_CURRENT =
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
